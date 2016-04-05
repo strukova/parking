8e460751c7721b8495bbc6ddbf01d0e4cd27ffa5
Elena Strukova <elena.belyaevskikh@gmail.com>
2016-04-05 21:45:04 +0200

    Vehicle Management

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ var parkingApp = angular.module('parkingApp', []);
 
 var ParkingCtrl = function() {
   this.levels = 5;
-  this.vehicles = [
-    { plate: 'X-123', type: 'Car' },
-    { plate: 'Y-4331', type: 'Car' },
-    { plate: 'Z-654', type: 'Motorbike' }
-  ];
+  this.slotsPerLevel = 10;
+  this.slots = new Array(this.levels);
+  for (var i = 0; i < this.levels; i++) {
+    this.slots[i] = new Array(this.slotsPerLevel);
+  }
+  for (var i = 0; i < this.levels; i++) {
+    for (var j = 0; j < this.slotsPerLevel; j++) {
+      this.slots[i][j] = true; // Each slot is empty in the beginning.
+    }
+  }
+  this.vehicles = [];
+
+  this.AddVehicle('X-123', 'Car');
+  this.AddVehicle('Y-321', 'Motorbike');
 };
 
 ParkingCtrl.prototype.LevelLabels = function() {
@@ -15,6 +24,24 @@ ParkingCtrl.prototype.LevelLabels = function() {
     result.push("Level " + (i + 1));
   }
   return result;
+};
+
+ParkingCtrl.prototype.AddVehicle = function(plate, type) {
+  for (var i = 0; i < this.levels; i++) {
+    for (var j = 0; j < this.slotsPerLevel; j++) {
+      if (this.slots[i][j]) {
+        this.slots[i][j] = false;
+        var vehicle = {"plate": plate, "type": type, "level": i, "slot": j};
+        this.vehicles.push(vehicle);
+        return;
+      }
+    }
+  }
+  this.ShowError("No free slots");
+};
+
+ParkingCtrl.prototype.ShowError = function(message) {
+  alert(message);
 };
 
 parkingApp.controller('ParkingCtrl', ParkingCtrl);
